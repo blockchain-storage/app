a01a0a3446d7bebfc946aff8a065966d177867bb
Clinton Cao <C.S.Cao@student.tudelft.nl>
2017-12-04 14:58:47 +0100

    Forgot to change the test.

--- app/src/androidTest/java/nl/tudelft/cs4160/trustchain_android/PeerAppToAppTest.java
+++ app/src/androidTest/java/nl/tudelft/cs4160/trustchain_android/PeerAppToAppTest.java
@@ -46,6 +46,6 @@ public class PeerAppToAppTest extends TestCase {
     public void testNotEqual() {
         PeerAppToApp peer1 = new PeerAppToApp("RANDOM", address);
         PeerAppToApp peer2 = new PeerAppToApp("MORERANDOM", address);
-        assertTrue(peer1.equals(peer2));
+        assertFalse(peer1.equals(peer2));
     }
 }
