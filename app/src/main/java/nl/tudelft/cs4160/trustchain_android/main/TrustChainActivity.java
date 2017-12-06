932af974ef38ef20dcec91ce46b7b431f77d85fd
Laurens Weijs <L.J.R.Weijs@student.tudelft.nl>
2017-12-06 14:37:21 +0100

    Logging + debugging for the input validation

--- app/src/main/java/nl/tudelft/cs4160/trustchain_android/main/TrustChainActivity.java
+++ app/src/main/java/nl/tudelft/cs4160/trustchain_android/main/TrustChainActivity.java
@@ -83,25 +83,31 @@ public class TrustChainActivity extends AppCompatActivity implements Communicati
 
     public void onClickConnect(View view) {
         String destinationIp = "";
+        String potentialPort = "";
         int destinationPort = 0;
         // input validation when the use adjusts the IP and/or port of the destination address.
         try{
             destinationIp = editTextDestinationIP.getText().toString();
-            if(destinationIp.length() < 6 || destinationIp.length() > 15){
+            String len = destinationIp.replace(".", "");
+            if(len.length() < 6 || len.length() > 15){
                 throw new Exception("IP is not of a valid length");
             }
             Object res = InetAddress.getByName(destinationIp);
-            if(!(res instanceof Inet4Address) || !(res instanceof Inet6Address)){
+            if(!(res instanceof Inet4Address) && !(res instanceof Inet6Address)){
+                Log.i("Destination IP Adress: ", res.toString());
                 throw new Exception("IP is not a valid IP4 or IP6 address.");
             }
-            String potentialPort = editTextDestinationPort.getText().toString();
+            potentialPort = editTextDestinationPort.getText().toString();
+            Toast.makeText(thisActivity, potentialPort, Toast.LENGTH_LONG);
+            Log.i("Potential Port: ", potentialPort);
             destinationPort = Integer.parseInt(potentialPort);
             if (destinationPort < 1024 || destinationPort > 5000) {
+                Log.i("Destination port: ", Integer.toString(destinationPort));
                 throw new Exception("Only use a range of valid ports.");
             }
         } catch (Exception e){
-            Toast.makeText(thisActivity, "The destination port or the IP adress is not a valid. Port: " + destinationPort + " IP: " + destinationIp, Toast.LENGTH_SHORT).show();
-            e.printStackTrace();
+            Toast.makeText(thisActivity, "The destination port or the IP adress is not a valid. Port: " + potentialPort + " IP: " + destinationIp, Toast.LENGTH_SHORT).show();
+//            e.printStackTrace();
         }
         peer = new Peer(null,destinationIp , destinationPort);
         communication.connectToPeer(peer);
