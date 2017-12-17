9acd5088a595456a4a80deb3a1d2dfee21a30e58
Laurens Weijs <L.J.R.Weijs@student.tudelft.nl>
2017-12-17 22:43:51 +0100

    Input validation for the bootstrap IP

--- app/src/main/java/nl/tudelft/cs4160/trustchain_android/main/BootstrapActivity.java
+++ app/src/main/java/nl/tudelft/cs4160/trustchain_android/main/BootstrapActivity.java
@@ -5,8 +5,13 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InetAddress;
 
 import nl.tudelft.cs4160.trustchain_android.R;
 
@@ -21,6 +26,15 @@ public class BootstrapActivity extends AppCompatActivity {
 
     public void onClickConnect(View view) {
         bootstrapView = (EditText) findViewById(R.id.bootstrap_IP);
+        try{
+            Object res = InetAddress.getByName(bootstrapView.getText().toString());
+            if(!(res instanceof Inet4Address) && !(res instanceof Inet6Address)){
+                Log.i("Destination IP Adress: ", res.toString());
+                throw new Exception("Bootstrap IP is not a valid IP4 or IP6 address.");
+            }
+        } catch (Exception e){
+             e.printStackTrace();
+        }
         Intent returnIntent = new Intent();
         returnIntent.putExtra("ConnectableAddress",bootstrapView.getText().toString());
         setResult(OverviewConnectionsActivity.RESULT_OK,returnIntent);
