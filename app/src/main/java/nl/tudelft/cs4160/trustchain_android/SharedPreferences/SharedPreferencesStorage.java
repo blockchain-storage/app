47e3dcd2a3eecc743ba0b331c593c3d1987209e2
TimBuckers <timbuckers@gmail.com>
2017-12-18 14:47:53 +0100

    redundant code

--- app/src/main/java/nl/tudelft/cs4160/trustchain_android/SharedPreferences/SharedPreferencesStorage.java
+++ app/src/main/java/nl/tudelft/cs4160/trustchain_android/SharedPreferences/SharedPreferencesStorage.java
@@ -21,28 +21,10 @@ public final class SharedPreferencesStorage {
         }
     }
 
-    public static int readSharedPreferencesInt(Context context, String key) {
-        SharedPreferences settings = context.getSharedPreferences(PREFS_NAME, 0);
-
-        if (settings.contains(key)) {
-            int object = settings.getInt(key, -1);
-            return object;
-        } else {
-            return -1;
-        }
-    }
-
     public static void writeSharedPreferences(Context context, String key, String value) {
         SharedPreferences settings = context.getSharedPreferences(PREFS_NAME, 0);
         SharedPreferences.Editor editor = settings.edit();
         editor.putString(key, value);
-        editor.apply();
-    }
-
-    public static void writeSharedPreferences(Context context, String key, int value) {
-        SharedPreferences settings = context.getSharedPreferences(PREFS_NAME, 0);
-        SharedPreferences.Editor editor = settings.edit();
-        editor.putInt(key, value);
         editor.apply();
     }
 
